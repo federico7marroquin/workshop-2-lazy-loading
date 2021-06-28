f055432242fad2b8ecfd14f36b58e1ba16d9da79
federico7marroquin <federico7marroquin@users.noreply.github.com>
2021-06-28 03:13:54 +0000

    deploy: 1d047f9c378e5b1ef821db55a2dac63548f7929c

---
+++ _dist_/index.js
@@ -0,0 +1,51 @@
+import { registerImage } from './lazy.js'
+
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+
+
+const baseUrl ='https://randomfox.ca/images/'
+const appNode = document.querySelector('#images')
+ const loadFoxyImage = () => {
+
+    const container = document.createElement('div')
+    container.className = 'p-4'
+
+    const image = document.createElement('img')
+    image.dataset.src = `${baseUrl}${Math.floor(Math.random()*121 + 1)}.jpg`
+    image.className = 'mx-auto'
+    image.width = '320'
+    image.alt = 'foxie'
+
+
+    container.append(image)
+           
+    appNode.append(container)
+    registerImage(container)
+ }
+
+ const cleanFoxyImages = () => {
+   [ ...appNode.childNodes].forEach(foxyImage => foxyImage.remove())
+ }
+
+ const button = document.createElement('button')
+ button.onclick = () => loadFoxyImage() 
+ button.innerHTML = 'Carga un zorrito'
+ button.className = 'mt-5 p-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block mx-auto'
+ 
+ const cleanButton = document.createElement('button')
+ cleanButton.onclick = () => cleanFoxyImages() 
+ cleanButton.innerHTML = 'Borrar zorritos'
+ cleanButton.className = 'mt-5 p-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded block mx-auto'
+ 
+ const buttonsDiv = document.createElement('div')
+ buttonsDiv.append(button, cleanButton)
+ 
+ appNode.insertAdjacentElement( 'beforebegin', buttonsDiv)
+
+
+
+
---
+++ _dist_/lazy.js
@@ -0,0 +1,25 @@
+
+const isIntersection = (entry) => {
+    return entry.isIntersecting 
+}
+
+const loadImage = (entry) => {
+    const container = entry.target
+    const image = container.firstChild
+    const url = image.dataset.src
+    image.src = url
+    observer.unobserve(container)
+}
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries 
+        .filter(isIntersection)
+        .forEach(loadImage)
+
+})
+
+export const registerImage = (image) => {
+    observer.observe(image)
+
+}
